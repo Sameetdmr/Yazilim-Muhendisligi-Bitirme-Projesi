7b54e55fcf2544480681556ce53d2ac2e540bdac
sameetdmr <dsamed568@gmail.com>
2020-03-28 17:21:01 +0300

    Rapor-2 Güncellemesi

--- lib/src/ui/home/home_screen.dart
+++ lib/src/ui/home/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:haber_deprem_app/src/bloc/home/home_bloc.dart';
+import 'package:haber_deprem_app/src/model/category/category.dart';
 import 'package:intl/intl.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -34,10 +35,12 @@ class HomeScreen extends StatelessWidget {
                     WidgetTitle(strToday),
                   ],
                 ),
-                // To something i here
+                SizedBox(height: 12.0),
+                WidgetCategory(),
               ],
             ),
-          )
+          ),
+          // Todo do something
         ],
       ),
     ));
@@ -96,6 +99,100 @@ class WidgetTitle extends StatelessWidget {
             )
           ]),
         ),
+      ),
+    );
+  }
+}
+
+class WidgetCategory extends StatefulWidget {
+  @override
+  _WidgetCategoryState createState() => _WidgetCategoryState();
+}
+
+class _WidgetCategoryState extends State<WidgetCategory> {
+  final listCategories = [
+    Category('', 'All'),
+    Category('assets/images/img_business.png', 'Business'),
+    Category('assets/images/img_entertainment.png', 'Entertainment'),
+    Category('assets/images/img_healt.png', 'Healt'),
+    Category('assets/images/img_science.png', 'Science'),
+    Category('assets/images/img_sport.png', 'Sport'),
+    Category('assets/images/img_technology.png', 'Technology'),
+  ];
+  int indexSelectedCategory = 0;
+  @override
+  void initState() {
+    final homeBloc = BlocProvider.of<HomeBloc>(context);
+    homeBloc.add(DataEvent(listCategories[indexSelectedCategory].title));
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final homeBloc = BlocProvider.of<HomeBloc>(context);
+    return Container(
+      height: 74.0,
+      child: ListView.builder(
+        shrinkWrap: false,
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (context, index) {
+          Category itemCategory = listCategories[index];
+          return Padding(
+            padding: EdgeInsets.only(
+              left: 16.0,
+              right: index == listCategories.length - 1 ? 16.0 : 0.0,
+            ),
+            child: Column(
+              children: <Widget>[
+                GestureDetector(
+                  onTap: () {
+                    //Todo yapılacaklar-2
+                  },
+                  child: index == 0
+                      ? Container(
+                          width: 48.0,
+                          height: 48.0,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Color(0xFFBDCDDE),
+                            border: indexSelectedCategory == index
+                                ? Border.all(color: Colors.white, width: 5.0)
+                                : null,
+                          ),
+                          child: Icon(Icons.apps, color: Colors.white),
+                        )
+                      : Container(
+                          width: 48.0,
+                          height: 48.0,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            image: DecorationImage(
+                              image: AssetImage(itemCategory.image),
+                              fit: BoxFit.cover,
+                            ),
+                            border: indexSelectedCategory == index
+                                ? Border.all(
+                                    color: Colors.white,
+                                    width: 5.0,
+                                  )
+                                : null,
+                          ),
+                        ),
+                ),
+                SizedBox(height: 8.0),
+                Text(
+                  itemCategory.title,
+                  style: TextStyle(
+                    fontSize: 14.0,
+                    color: Color(0xFF325384),
+                    fontWeight: indexSelectedCategory == index ? FontWeight.bold : FontWeight.normal
+                  ),
+                )
+              ],
+            ),
+          );
+        },
+        itemCount: listCategories.length,
       ),
     );
   }
